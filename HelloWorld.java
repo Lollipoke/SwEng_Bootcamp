975361e0104ff8ca992a4c2da81be7b70986e242
Mélissa <melissa.gehring@epfl.ch>
2020-09-17 18:34:46 +0200

    Finalised version

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,12 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, SwEng !");
-    System.out.println("The ultimate question of Life, The Universe, and Everything is: " + computeUltimateQuestion());
+    System.out.println(computeUltimateQuestion());
+  	System.out.println("That's it !");
   }
 
   public static String computeUltimateQuestion() {
-	return "still searching...";
-    System.out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
+	return "The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer();
   }
 
   public static int computeUltimateAnswer() {
